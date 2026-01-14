15425817c99505e59542c8a104cc8ded14deadd8
zbf-collaborator-01 <152584861+zbf-collaborator-01@users.noreply.github.com>
2026-01-14 11:41:20 +0900

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../lib/auth.php';
-logout_all();
-    $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-    header('Location: ' . ($base ?: '/') . '/index.php');
-exit;
